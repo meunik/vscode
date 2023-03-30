159a5996347a2cc93613ff26c066e8b2e536a9ac
meunik <meunik@live.com>
2023-03-29 22:17:43 -0300

    urlProd

--- src/store/api.js
+++ src/store/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const urlDev = 'http://localhost:3000'
-// const urlProd = 'http://localhost:3000'
+const urlProd = 'https://meunik.github.io/'
 
 const api = axios.create({
     baseURL: process.env.NODE_ENV === 'development' ? urlDev : urlProd,
